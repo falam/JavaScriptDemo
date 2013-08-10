5a21c34d126c983fbace11c865cbe89cc8290679
Farhan Alam <alam.farhan@gmail.com>
2013-08-11 01:42:00 +1000

    Clean up nested repo.

---
+++ TemperatureConvert/Controllers/DemoController.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Web.Mvc;
+
+namespace TemperatureCalculator.Controllers
+{
+    public class DemoController : Controller
+    {
+        //
+        // GET: /Demo/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        //[HttpGet]
+        public ActionResult FormDemo()
+        {
+            if (Request.IsAjaxRequest())
+            {
+                return Json(new { Date = DateTime.Now.ToString() });
+            }
+            string name = HttpContext.Request.Form["name"];
+
+            if (!string.IsNullOrEmpty(name))
+            {
+                ViewBag.Message = name;
+                return View();
+            }
+            if (!HttpContext.Request.HttpMethod.Equals("GET", StringComparison.CurrentCultureIgnoreCase))
+                ViewBag.Message = "Your name?";
+
+            return View();
+        }
+
+        public ActionResult TemperatureConvert()
+        {
+            if (Request.IsAjaxRequest())
+            {
+                string tempInC = HttpContext.Request.Form["tempInC"];
+                double input, result;
+
+                if (Double.TryParse(tempInC, out input))
+                {
+                    result = ((input * 9) / 5) + 32;
+                    return Json(new {Result = result});
+                }
+            }
+
+            //if (!string.IsNullOrEmpty(name))
+            //{
+            //    ViewBag.Message = name;
+            //    return View("FormDemo");
+            //}
+            //if (!HttpContext.Request.HttpMethod.Equals("GET", StringComparison.CurrentCultureIgnoreCase))
+            //    ViewBag.Message = "Your name?";
+
+            return View("FormDemo");
+        }
+
+        /*
+        [HttpPost]
+        public ActionResult FormDemo()
+        {
+            return View();
+        }*/
+    }
+}
---
+++ TemperatureConvert/Models/DemoModel.cs
@@ -0,0 +1,20 @@
+﻿namespace TemperatureCalculator.Models
+{
+    public class DemoModel
+    {
+        private string _firstName;
+        private string _lastName;
+
+        public string LastName
+        {
+            get { return _lastName; }
+            set { _lastName = value; }
+        }
+
+        public string FirstName
+        {
+            get { return _firstName; }
+            set { _firstName = value; }
+        }
+    }
+}
