ae604bc4179a8f31d04b97fd8102994b246da190
Farhan Alam <alam.farhan@gmail.com>
2013-08-11 01:42:00 +1000

    Clean up nested repo.

---
+++ TemperatureConvert/Scripts/Demo.js
@@ -0,0 +1,42 @@
+﻿(function () {
+    $(document).ready(function () {
+        var result = $("#result").text();
+        if (result === undefined || result === null || result.length <= 0 || result === "Your name?") {
+            $("#greeting").text("Please enter your name.");
+            $("#result").hide();
+        }
+        else {
+            $("#result").hide();
+            $("#greeting").text("Good Day " + result);
+        }
+    });
+
+    var highlight = function () {
+        $("#greeting").css("color", "red");
+    };
+
+    $("#greeting").click(highlight);
+
+    $("#ajax").click(function () {
+        var result = $("#greeting").text();
+        $.ajax({
+            url: "/Demo/FormDemo",
+            type: 'POST',
+            dataType: "json",
+            data: { "msg": result },
+            success: function (response) { $("#result").show().text("Time: " + response.Date); }
+        });
+    });
+
+    $("#convert").click(function () {
+        var input = $("#temp").val();
+        $.ajax({
+            url: "/Demo/TemperatureConvert",
+            type: 'POST',
+            dataType: "json",
+            data: { "tempInC": input },
+            success: function (response) { $("#tempResult").show().text( response.Result); }
+        });
+    });
+    
+})();
